851d86f4844b4453d38831808e7177dde9bb4d52
julaicc <juliocc@MacBook-Pro-de-Julz.local>
2024-06-05 12:10:28 -0600

    ya casi

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,13 @@
 
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    let metadata=data.metadata;
+
+  var metadata=data.metadata;
+    console.log("Metadata: ", data.metadata);
+
   
     // Filter the metadata for the object with the desired sample number  
-  let result=metadata.filter(sampleObj => sampleObj.id== sample)[0];
+    var result=metadata.filter(sampleObj => sampleObj.id== sample)[0];
     console.log("selected Metadata:", result);
 
 
@@ -19,7 +22,7 @@ function buildMetadata(sample) {
     Object.entries(result).forEach(([key, value])=>{
       panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
-  }).catch(error => console.error('Error fetching metadata:', error)); 
+  }); 
 
 }
 
@@ -27,27 +30,23 @@ function buildMetadata(sample) {
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    let samples=data.samples;
+    var samples=data.samples;
+    console.log("Samples Data:", data.samples);
+    
     
     // Filter the samples for the object with the desired sample number
-    let result=samples.filter(SampleObj => sampelObj.id==sample)[0];
+    var result=samples.filter(sampleObj => sampleObj.id==sample)[0];
+    console.log("Selected Sample Data:", result);
 
     
     // Get the otu_ids, otu_labels, and sample_values
-    let otu_ids = result.otu_ids;
-    let otu_labels = result.otu_labels;
-    let sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
 
     // Build a Bubble Chart
-    let bubbleLayout = {
-      title: "Bacteria Cultures Per Sample",
-      margin: { t: 0 },
-      hovermode: "closest",
-      xaxis: { title: "OTU ID" },
-      margin: { t: 30 }
-    };
-    let bubbleData = [
+    var bubbleData = [
       {
         x: otu_ids,
         y: sample_values,
@@ -60,33 +59,42 @@ function buildCharts(sample) {
         }
       }
     ];
+
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+  
+
      // Render the Bubble Chart
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
 
-    let barData = [
-      {
+    var barData = [{
         y: yticks,
         x: sample_values.slice(0, 10).reverse(),
         text: otu_labels.slice(0, 10).reverse(),
         type: "bar",
         orientation: "h",
-      }
-    ];
-    let barLayout = {
+      }];
+
+    var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       margin: { t: 30, l: 150 }
     };
 
     // Render the Bar Chart
     Plotly.newPlot("bar", barData, barLayout);
-  }).catch(error => console.error('Error fetching samples:', error));
+  });
 }
 
 
@@ -95,11 +103,11 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-    let names = data.names;
+    var names = data.names;
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
-    let selector = d3.select("#selDataset");
+    var selector = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
@@ -113,13 +121,13 @@ function init() {
 
 
     // Get the first sample from the list
-    let firstSample = names[0]
+    var firstSample = names[0];
 
 
     // Build charts and metadata panel with the first sample
     buildCharts(firstSample);
     buildMetadata(firstSample);
-  }).catch(error => console.error('Error fetching names:', error));
+  });
 }
 
 
